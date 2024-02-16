1a58204f84d6595c5873467bcd2b5ed01322226e
Juan Sebastian Giraldo <juans.giraldo.e@gmail.com>
2024-02-16 12:10:34 -0500

    [Finish] - Data creation test in the database.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
+let DB;
+
 document.addEventListener('DOMContentLoaded', ()=>{
   crmDB();
-})
+  setTimeout(() => {
+    crearCliente();
+  }, 5000);
+});
 
 function crmDB() {
   //Crear base de datos version 1.0
@@ -12,18 +17,39 @@ function crmDB() {
   //Si se creo bien.
   crmDB.onsuccess = function () {
     console.log('Base de datos creada.');
+    DB = crmDB.result;
   };
   //Configuracion de la base de datos.
   crmDB.onupgradeneeded = function (e) {
     const db = e.target.result;
-    const objecStore = db.createObjectStore('crm', {
+    const objectStore = db.createObjectStore('crm', {
       keyPath: 'crm',
       autoIncrement: true,
     });
     //Definir las columnas.
-    objecStore.createIndex('nombre', 'nombre', {unique: false});
-    objecStore.createIndex('email', 'email', {unique: true});
-    objecStore.createIndex('telefono', 'telefono', {unique: false});
+    objectStore.createIndex('nombre', 'nombre', {unique: false});
+    objectStore.createIndex('email', 'email', {unique: true});
+    objectStore.createIndex('telefono', 'telefono', {unique: false});
     console.log('Columnas creadas.');
   };
+};
+
+function crearCliente() {
+  let transaction = DB.transaction(['crm'], 'readwrite');
+  transaction.oncomplete = function () {
+    console.log('Transaccion completada.');
+  };
+  transaction.onerror = function () {
+    console.log('Hubo un error en la transaccion.');
+  };
+  const objectStore = transaction.objectStore('crm');
+
+  const nuevoCliente = {
+    telefono: 3105,
+    nombre: 'Juan',
+    email: 'correo@correo.com',
+  };
+
+  const peticion = objectStore.add(nuevoCliente);
+  console.log(peticion);
 };
